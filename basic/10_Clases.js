9509a3da70aae626bdd365999b9ee56e74d33fc0
ericksebas24 <60784826+ericksebas24@users.noreply.github.com>
2021-06-23 16:23:03 -0500

    implementar getters, setters y realizar una implementacion

--- basic/10_Clases.js
+++ basic/10_Clases.js
@@ -1,33 +1,33 @@
-//Como se hacia antes 
-function Rectangulo(base, altura){
-    this.base = base
-    this.altura = altura
-}
+// //Como se hacia antes 
+// function Rectangulo(base, altura){
+//     this.base = base
+//     this.altura = altura
+// }
 
-Rectangulo.prototype.CalcularArea = function() {
-    return this.base * this.altura
-}
+// Rectangulo.prototype.CalcularArea = function() {
+//     return this.base * this.altura
+// }
 
-var rectangulo = new Rectangulo(6, 3)
+// var rectangulo = new Rectangulo(6, 3)
 
-console.log(rectangulo.CalcularArea())
-console.log(rectangulo.base)
+// console.log(rectangulo.CalcularArea())
+// console.log(rectangulo.base)
 
-//  Declaracion de clases
-class Rectangulo2{
-    constructor(base, altura) {
-        this.base = base 
-        this.altura = altura
-    }
+// //  Declaracion de clases
+// class Rectangulo2{
+//     constructor(base, altura) {
+//         this.base = base 
+//         this.altura = altura
+//     }
 
-    CalcularArea(){
-        return this.base * this.altura
-    }
-}
+//     CalcularArea(){
+//         return this.base * this.altura
+//     }
+// }
 
-var Rectangulo2 = new Rectangulo2 (6, 3)
+// var Rectangulo2 = new Rectangulo2 (6, 3)
 
-console.log(Rectangulo2.CalcularArea())
+// console.log(Rectangulo2.CalcularArea())
 //Clase persona 
 //implementar getters, setters y realizar una implementacion 
 //implementar metodos estaticos -clase punto, function distancia
@@ -35,4 +35,56 @@ console.log(Rectangulo2.CalcularArea())
 
 /*
 DEBER
+*/
+class Persona{
+    constructor(nombre, apellido, email, edad){
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._email = email;
+        this._edad = edad;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get email(){
+        return this._email;
+    }
+
+    set email(email){
+        this._email = email;
+    }
+
+    get edad(){
+        return this._edad;
+    }
+    
+    set edad(edad){
+        this._edad = edad;
+    }
+}
+
+let edward = new Persona('Erick', 'Fernández', 'ericksebas13jer@gmail.com', 21);
+
+console.log(edward);
+
+console.log(edward.nombre);
+
+edward.nombre = 'Sebas';
+console.log(edward.nombre);
+
+/*
+
 */
