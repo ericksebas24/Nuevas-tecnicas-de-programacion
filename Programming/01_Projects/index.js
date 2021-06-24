b71d8f1d337a451cd3284e53af9e28539f1451ae
ericksebas24 <60784826+ericksebas24@users.noreply.github.com>
2021-06-24 12:05:26 -0500

    24/06/2021

--- Programming/01_Projects/index.js
+++ Programming/01_Projects/index.js
@@ -1,12 +1,45 @@
+const express = require('express');
+
+
 //genero una aplicacion express
 
-const express = require('express'); 
 const app = express()
 // endpoint 
 app.get('/',(req, res) => {
     res.send("Hola mundo con ExpressJS")
 })
 
+app.get("/saludo",(req, res) =>{
+    //req.query
+    // console.log(req.query);
+    const { query: {nombre, apellido} } = req;
+    // const nombre = req.query.nombre
+    // const apellido = req.query.apellido
+
+    // const user={
+    //     nombre: "Gio",
+    //     apellido: "Ortiz"
+
+    // }
+    // user.nombre
+    // user.apellido
+    // req.query.nombre
+
+
+    res.json({
+        saludo: `Hola soy ${nombre} ${apellido}`,
+    });
+})
+
+app.get('/saludo/:nombre', (req, res) =>{
+
+    const { params: { nombre } } = req
+    console.log(req.params);
+
+    res.json({
+        nombre: nombre,
+    })
+})
 app.listen(3000, ()=>{
     console.log("Servidor escuchando en htpp://localhost:3000");
 })
---
+++ Programming/01_Projects/routes/index.js
@@ -0,0 +1,18 @@
+//traemos el paquete
+const { Router } = require('express');
+
+//inicializamos router
+const router = Router()
+
+//creacion de los endpoints
+
+
+//exportamos las rutas 
+module.exports.RouterIndex = router
+
+
+// module = {
+//     exports: {
+//         RouterIndex: router 
+//     }
+// }
