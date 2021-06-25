165ca2d5cfce4f77f0c8e4cc21548bbcac7eef8e
ericksebas24 <60784826+ericksebas24@users.noreply.github.com>
2021-06-25 12:06:53 -0500

    25/06/2021

--- Programming/01_Projects/routes/index.js
+++ Programming/01_Projects/routes/index.js
@@ -1,18 +1,38 @@
 //traemos el paquete
+
 const { Router } = require('express');
 
-//inicializamos router
+//inicializamos el router
+
 const router = Router()
 
-//creacion de los endpoints
+//creacion de los endpoint
+router
+    .get("/",(req, res) => {
+    res.send("Hola mundo con ExpressJS");
+    })
+    .get("/saludo",(req, res) =>{
+
+        const { query: {nombre, apellido} } = req;
+        res.json({
+            saludo: `Hola soy ${nombre} ${apellido}`,
+        });
+    })
+    .get('/saludo/:nombre', (req, res) =>{
+        const { params: { nombre } } = req
+        console.log(req.params);
+        res.json({
+            nombre: nombre,
+        })
+    })
 
 
-//exportamos las rutas 
+
+//exportamos la rutas
 module.exports.RouterIndex = router
-
 
 // module = {
 //     exports: {
-//         RouterIndex: router 
+//         RouterIndex: router
 //     }
 // }
